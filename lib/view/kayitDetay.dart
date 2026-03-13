23c052e4e4cc81ed019e77b75972914b97f38982
Hakan YARIŞ İş Bilgisayarı <hakanayris@hotmail.com>
2026-03-13 13:27:25 +0300

    13.02.2026 iş

--- lib/view/kayitDetay.dart
+++ lib/view/kayitDetay.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:not_defteri/model/kayitlar.dart';
+import 'package:flutter/services.dart';
 
 class KayitDetay extends StatefulWidget {
   final Kayit kayit;
@@ -19,5 +20,40 @@ class _KayitDetayState extends State<KayitDetay> {
     return AppBar(title: Text(widget.kayit.kayitAdi.toString()));
   }
 
-  Widget? _buildBody() {}
+  Widget? _buildBody() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(flex: 1, child: Text("Kullanici Adı :")),
+              Expanded(flex: 3, child: Text("${widget.kayit.kullaniciAdi}")),
+            ],
+          ),
+          SizedBox(height: 3),
+          Row(
+            children: [
+              Text("Şifre :"),
+              Text("${widget.kayit.sifre}"),
+              TextButton(
+                onPressed: () {
+                  // Kopyalama işlemi burada gerçekleşiyor
+                  Clipboard.setData(
+                    ClipboardData(text: widget.kayit.sifre),
+                  ).then((_) {
+                    // Kullanıcıya bilgi vermek her zaman iyi bir deneyimdir
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Metin panoya kopyalandı!")),
+                    );
+                  });
+                },
+                child: Text("Kopyala"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
